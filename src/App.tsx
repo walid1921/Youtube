65f76754494820ecc7be85d7eb01053dc4ae2517
user.walid1921 <user.kouiderayadwalid@gmail.com>
2024-03-17 20:20:24 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import { HeaderPage } from './layouts/HeaderPage.tsx'
+
+export default function App() {
+  return (
+    <h1 className="max-h-screen flex flex-col">
+      <HeaderPage  />
+    </h1>
+  )
+}
